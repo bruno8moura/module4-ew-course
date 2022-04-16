6c5d1a291a28aa276e470112d280da891871055e
Bruno Moura <bruno8moura@gmail.com>
2022-04-16 04:54:26 -0300

    test: add DateFormat.test.js

---
+++ test/DateFormat.test.js
@@ -0,0 +1,52 @@
+import mocha from 'mocha'
+
+import chai from 'chai'
+
+import Person from '../src/domain/Person/index.js'
+import Internationalization from '../src/infra/Internationalization/index.js'
+import DateFormat from '../src/utils/DateFormat/index.js'
+const { describe, it } = mocha
+const { expect } = chai
+
+describe('DateFormat', () => {
+  it('should return a person instance from a string', () => {
+    const person = Person.generateInstanceFromString(
+      '1 Bike,Carro 20000 2020-01-01 2020-02-01',
+      new Internationalization(),
+      new DateFormat()
+    )
+
+    const expected = {
+      from: '2020-01-01',
+      to: '2020-02-01',
+      vehicles: ['Bike', 'Carro'],
+      kmTraveled: '20000',
+      id: '1'
+    }
+
+    const { id, from, to, vehicles, kmTraveled } = person
+
+    const p = { id, from, to, vehicles, kmTraveled }
+
+    expect(p).to.be.deep.equal(expected)
+  })
+
+  it('should format person values', () => {
+    const person = Person.generateInstanceFromString(
+      '1 Bike,Carro 20000 2020-01-01 2020-02-01',
+      new Internationalization(),
+      new DateFormat()
+    )
+
+    const result = person.formatted('pt-BR')
+    const expected = {
+      id: 1,
+      vehicles: 'Bike e Carro',
+      kmTraveled: '20.000 km',
+      from: '01 de janeiro de 2020',
+      to: '01 de fevereiro de 2020'
+    }
+
+    expect(result).to.be.deep.equal(expected)
+  })
+})
