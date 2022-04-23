e2af21de447b0a3221dade3ca830ae2b7a8e37fc
Bruno Moura <bruno8moura@gmail.com>
2022-04-23 07:17:38 -0300

    refactor: change class style

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import Person from './domain/Person/index.js'
 import database from './infra/database/database.json'
 import Internationalization from './infra/Internationalization/index.js'
 import Table from './infra/Table/index.js'
-import { createCustomTerminal } from './infra/Terminal/customTerminal.js'
+import Terminal from './infra/Terminal/index.js'
 import TerminalController from './presentation/controllers/TerminalController/index.js'
 import DateFormat from './utils/DateFormat/index.js'
 import repository from './infra/repository/index.js'
 
 const DEFAULT_LANG = 'pt-BR'
-const customTerminal = createCustomTerminal()
+const customTerminal = new Terminal().create()
 const data = database.map(item => new Person(item, new Internationalization(), new DateFormat()))
 const table = new Table({language: DEFAULT_LANG, data})
 
