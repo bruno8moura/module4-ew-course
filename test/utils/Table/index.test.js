dc4260fc1bb08ef47d53886f20bb1b431e665ac4
Bruno Moura <bruno8moura@gmail.com>
2022-04-26 05:37:52 -0300

    test: convert table fields to object fields

--- test/utils/Table/index.test.js
+++ test/utils/Table/index.test.js
@@ -15,4 +15,22 @@ describe(Table.name, () => {
     const expectedData = [addItem]
     expect(data).to.be.deep.equal(expectedData)
   })
+
+  it('should convert table fields to object fields', () => {
+    const tableFields = ['Field A', 'Field B', 'Field C']
+    const anObject = {
+      fieldA: 'a',
+      fieldB: 'b',
+      fieldC: 'c'
+    }
+    const data = [anObject]
+    const sut = new Table({ data, formatter: {}, tableFields })
+
+    const [itemA, itemB, itemC] = sut._buildColumns()
+
+    const [keyA, keyB, keyC] = Object.keys(anObject)
+    expect(itemA.field).to.be.equal(keyA)
+    expect(itemB.field).to.be.equal(keyB)
+    expect(itemC.field).to.be.equal(keyC)
+  })
 })
